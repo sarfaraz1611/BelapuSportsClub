c04bf77c47ec9450a1ddd9523984c48b9335fc43
Mohammed sarfaraz <mohammedsarfaraz18@gmail.com>
2024-10-29 01:08:18 +0530

    yml

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/BelapuSportsClub/",
+  base: "/BelapuSportsClub",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
