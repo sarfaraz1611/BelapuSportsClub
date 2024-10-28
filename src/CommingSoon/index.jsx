453558d7382739a985ea20d2e6d675d01f16b48b
Mohammed sarfaraz <mohammedsarfaraz18@gmail.com>
2024-10-29 00:36:12 +0530

    meteors functionality

--- src/CommingSoon/index.jsx
+++ src/CommingSoon/index.jsx
@@ -2,9 +2,9 @@ import Meteors from "../components/ui/meteors";
 
 export function MeteorDemo() {
   return (
-    <div className=" flex !h-[100dvh] w-full flex-col items-center justify-center ">
+    <div className="relative flex h-[90vh] w-full  flex-col items-center justify-center overflow-hidden bg-background   ">
       <Meteors number={60} />
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center  text-2xl md:text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Belapu Sports Club
       </span>
       <span className=" mt-6 pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-2xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
