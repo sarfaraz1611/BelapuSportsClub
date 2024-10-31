52df46570c8582e6e516a9505fb5ccefaa47eeaf
Mohammed sarfaraz <mohammedsarfaraz18@gmail.com>
2024-10-31 22:46:46 +0530

    versal

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import path from "path";
+// import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  // plugins: [react()],
+  // base: "/BelapuSportsClub",
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+
+  base: "./", // Adjust as necessary
   plugins: [react()],
-  base: "/BelapuSportsClub",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
